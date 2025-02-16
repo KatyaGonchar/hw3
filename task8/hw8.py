57b8aa96b0b1f326d8da0735e79b1b626d7b767b
KatyaGonchar <katyahonchar95@gmail.com>
2025-02-16 22:33:33 +0200

    Added counting of missing sizes

--- task8/hw8.py
+++ task8/hw8.py
@@ -28,7 +28,12 @@ a = [6, 2, 3, 8]
 m1 = min(a)
 m2 = max(a)
 b = 0
+c = ""
 for i in range(m1, m2 + 1):
     if i not in a:
-        b += 1
-print(b)
+        if c == "":
+            c = str(i)
+        else:
+            c = c + ", " + str(i)
+        b +=1
+print("Всего нет статуй:", b, "\nУ Вас отсутствуют статуи размеров:", c)
