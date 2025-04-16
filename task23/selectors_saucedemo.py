6b4a657a5ec0adac69196486fa53298a2c9f6c83
KatyaGonchar <katyahonchar95@gmail.com>
2025-04-16 22:55:38 +0300

    fixed linters error

--- task23/selectors_saucedemo.py
+++ task23/selectors_saucedemo.py
@@ -1,37 +1,35 @@
-'''
-Login page:
-1. Enter username
-selector: #user-name
-2. Enter password
-selector: #password
-3. Press "Login" button
-selector: #login-button
-
-Products page
-1. Add item to cart. Press Add to Cart button (example: Sauce Labs Backpack)
-selector: #add-to-cart-sauce-labs-backpack
-2. Go to the cart selector
-selector: .shopping_cart_link
-
-Your Cart page
-1. Click "Checkout" button
-selector: #checkout
-
-Checkout: Your Information page
-1. Enter First Name
-selector: #first-name
-2. Enter Last Name
-selector: #last-name
-3. Enter Zip/Postal Code
-selector: #postal-code
-4. Click "Continue" button
-selector: #continue
-
-Checkout: Overview page
-1. Click "Finish" button
-selector: #finish
-
-Checkout: Complete! page
-1. Click "Back Home" button
-selector: #back-to-products
-'''
+# Login page:
+# 1. Enter username
+# selector: #user-name
+# 2. Enter password
+# selector: #password
+# 3. Press "Login" button
+# selector: #login-button
+#
+# Products page
+# 1. Add item to cart. Press Add to Cart button (example: Sauce Labs Backpack)
+# selector: #add-to-cart-sauce-labs-backpack
+# 2. Go to the cart selector
+# selector: .shopping_cart_link
+#
+# Your Cart page
+# 1. Click "Checkout" button
+# selector: #checkout
+#
+# Checkout: Your Information page
+# 1. Enter First Name
+# selector: #first-name
+# 2. Enter Last Name
+# selector: #last-name
+# 3. Enter Zip/Postal Code
+# selector: #postal-code
+# 4. Click "Continue" button
+# selector: #continue
+#
+# Checkout: Overview page
+# 1. Click "Finish" button
+# selector: #finish
+#
+# Checkout: Complete! page
+# 1. Click "Back Home" button
+# selector: #back-to-products
